2f764dcf61489f8cd469006897859eea21efdc6c
chriscohoat <chris.cohoat@gmail.com>
2023-11-10 13:22:57 -0500

    Forced CUDA device.

--- speech_changer.py
+++ speech_changer.py
@@ -10,10 +10,14 @@ import argparse
 from tqdm import tqdm
 import json
 
+import torch
+
+torch.cuda.set_device(0)
+
 
 # Only for video with one person
 def update_voice(voice_filename, video_filename, output_filename):
-    with open('config.json', 'r') as f:
+    with open("config.json", "r") as f:
         config = json.load(f)
 
     dereverb = MDXNetDereverb(15)
@@ -22,49 +26,53 @@ def update_voice(voice_filename, video_filename, output_filename):
     lip_sync = LipSync()
 
     orig_clip = VideoFileClip(video_filename, verbose=False)
-    original_audio_file = temp_manager.create_temp_file(suffix='.wav').name
+    original_audio_file = temp_manager.create_temp_file(suffix=".wav").name
     orig_clip.audio.write_audiofile(
-        original_audio_file, codec='pcm_s16le', verbose=False, logger=None)
+        original_audio_file, codec="pcm_s16le", verbose=False, logger=None
+    )
 
     dereverb_out = dereverb.split(original_audio_file)
-    noise_audio = AudioSegment.from_file(
-        dereverb_out['noise_file'], format='wav')
+    noise_audio = AudioSegment.from_file(dereverb_out["noise_file"], format="wav")
 
     frames = dict()
-    for frame_id, frame in tqdm(enumerate(orig_clip.iter_frames()), desc='Processing frames'):
-        frames[frame_id] = {
-            'frame': frame,
-            'has_face': False
-        }
-        detections = face_detector.detect(frame, config['DET_TRESH'])
+    for frame_id, frame in tqdm(
+        enumerate(orig_clip.iter_frames()), desc="Processing frames"
+    ):
+        frames[frame_id] = {"frame": frame, "has_face": False}
+        detections = face_detector.detect(frame, config["DET_TRESH"])
         if detections:
             face, bbox = detections[0]
-            frames[frame_id]['has_face'] = True
-            frames[frame_id]['face'] = face
-            frames[frame_id]['bbox'] = bbox
+            frames[frame_id]["has_face"] = True
+            frames[frame_id]["face"] = face
+            frames[frame_id]["bbox"] = bbox
 
     frames = lip_sync.sync(frames, voice_filename, orig_clip.fps)
     temp_result_avi = to_avi(frames, orig_clip.fps)
 
-    noise_audio_wav = temp_manager.create_temp_file(suffix='.wav').name
-    noise_audio.export(noise_audio_wav, format='wav')
+    noise_audio_wav = temp_manager.create_temp_file(suffix=".wav").name
+    noise_audio.export(noise_audio_wav, format="wav")
 
     combined_audio = combine_audio(voice_filename, noise_audio_wav)
 
     merge(combined_audio, temp_result_avi, output_filename)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Combine an audio file and a video file into a new video file')
-    parser.add_argument('voice_filename', help='path to voice file')
-    parser.add_argument('video_filename', help='path to video file')
-    parser.add_argument('-o', '--output_filename', default='output.mp4',
-                        help='output file name (default: output.mp4)')
+        description="Combine an audio file and a video file into a new video file"
+    )
+    parser.add_argument("voice_filename", help="path to voice file")
+    parser.add_argument("video_filename", help="path to video file")
+    parser.add_argument(
+        "-o",
+        "--output_filename",
+        default="output.mp4",
+        help="output file name (default: output.mp4)",
+    )
     args = parser.parse_args()
 
     update_voice(
         video_filename=args.video_filename,
         voice_filename=args.voice_filename,
-        output_filename=args.output_filename
+        output_filename=args.output_filename,
     )
